d3b8d8cc7d850bb7a80ad72b5dcf7c21e1aba1bc
kotkotei <kotkotei@yandex.ru>
2021-04-30 10:59:16 +0700

    crigejeg

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//36
+            int a = 100;
+            int n = 0;
+            while (n <= 10)
+            {
+                if (a % 10 == 7 || a % 9 == 0)
+                {
+                    n++;
+                    Console.WriteLine(a);
+                }
+                a = a + 1;
+
+
+                
+                
+            }
+                    Console.ReadKey();
+        }
+    }
+}
+
